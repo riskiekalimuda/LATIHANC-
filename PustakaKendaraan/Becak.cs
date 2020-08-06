e141c65392190153fe512ff3843542df1a6160ef
riskiekalimuda <riskie.kalimuda@gmail.com>
2020-08-06 21:06:34 +0700

    Tambah fungsi jalankan

--- PustakaKendaraan/Becak.cs
+++ PustakaKendaraan/Becak.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PustakaAntarMuka;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace PustakaKendaraan
 {
-    class Becak
+    public class Becak : IKendaraan
     {
+        public void Jalankan()
+        {
+            Console.WriteLine("Menjalankan Sepeda: ");
+            Console.WriteLine("Mengayuh Sepeda");
+        }
     }
 }
